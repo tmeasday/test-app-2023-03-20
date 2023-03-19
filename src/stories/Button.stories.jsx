14f856200f8b482d46acb8bc2e734c07e0d35e5e
Tom Coleman <tom@chromatic.com>
2023-03-20 10:21:27 +1100

    First commit

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,61 @@
+import React, {useEffect} from 'react';
+
+import { Button } from './Button';
+
+const Wrapper = (storyFn) => {
+  useEffect(() => {
+    const logOnResize = () => {
+      const logDiv = document.querySelector("#resize-log");
+      logDiv.innerHTML += `Resized to ${window.innerWidth} / ${window.innerHeight} <br />`;
+    };
+    window.addEventListener("resize", logOnResize);
+    return () => {
+      window.removeEventListener("resize", logOnResize);
+    };
+  }, []);
+
+  return (
+    <div style={{ height: "100vh", padding: "10%", position: "relative" }}>
+      <div id="resize-log" data-testid="resize-log"></div>
+      {storyFn()}
+    </div>
+  );
+};
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  decorators: [Wrapper],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
